39959cacaff1a4ed8383c5fe98ca39b468e4b7d7
yonelamatha <yonelamatha@gmail.com>
2022-09-08 14:34:30 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
 
-int main(){
-
-	puts("Programming is like building a multilingual puzzle\n");
+int main(void){ puts("\"Programming is like building a multilingual puzzle\n");
 
 	return 0;
 }	
