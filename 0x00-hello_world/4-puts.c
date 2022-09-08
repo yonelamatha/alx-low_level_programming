471e7a8fc25b3eeb86dbb33691544eb3613219e4
yonelamatha <yonelamatha@gmail.com>
2022-09-08 16:33:59 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
+
+/**
+* print the main function
+*
+* this programme print "Programming is like building a multilingual puzzle
+*
+* return 0
+*/
 int main(void)
-{
-       	puts("\"Programming is like building a multilingual puzzle");
+{	
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
