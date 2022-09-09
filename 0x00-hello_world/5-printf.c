f41f2a0b450b6144c2bf807a7d6b7f8042d5e972
yonelamatha <yonelamatha@gmail.com>
2022-09-08 17:19:22 -0700

    print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+* print the main function
+*
+* The proramme print with proper grammar, but the outcome is a piece of art,
+*
+* return 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
