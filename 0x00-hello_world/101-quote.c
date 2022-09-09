774ada0b110e00cc95dbf4a3f6fce70d18888c85
yonelamatha <yonelamatha@gmail.com>
2022-09-08 19:23:27 -0700

    intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
 /**
-* main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
-* Return: Always 1.
-*/
-int main(voidi)
-{	write(2,
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-		59);
+ *
+ * main - Prints out the last part of a quote in the standard error.
+ *
+ * Return: 1 if success.
+ *
+ */
+int main(void)
+
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
-}	
+
+}
