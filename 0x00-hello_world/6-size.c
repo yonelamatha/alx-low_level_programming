d9bbad1ee126d720a421cad50fdb56f4736231e5
yonelamatha <yonelamatha@gmail.com>
2022-09-12 13:02:04 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,8 @@
 /**
  * main- print main function
  *
- * C program that prints the size of various types on the computer it is compiled and run on
+ * C program that prints the size of various types\
+ * on the computer it is compiled and run on
  *
  * return 0
  */
@@ -14,11 +15,11 @@ int main(void)
 	long long int c;
 	char d;
 	float f;
-	
+
 	printf("Size of a char: %lu byte(s)\n", sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", sizeof(a));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(b));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 	return (0);
-}	
+}
