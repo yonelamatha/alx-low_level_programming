62406e944e3199a4e19e82f724042ff00a8125c9
yonelamatha <yonelamatha@gmail.com>
2022-09-12 16:01:27 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,7 +13,7 @@
 int main(void)
 
 {
-	write(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n);
+	write(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 
 }
