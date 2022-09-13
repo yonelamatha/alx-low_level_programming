55ce574819143e90ba1584c9ccc5214c32648d5e
yonelamatha <yonelamatha@gmail.com>
2022-09-13 14:49:19 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - a function that prints 10 times the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Succes)
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int c, j;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (c = 'a'; c <= 'z')
+
+		_putchar(c);
+	}
+	_putchar('\n')
+
+}
