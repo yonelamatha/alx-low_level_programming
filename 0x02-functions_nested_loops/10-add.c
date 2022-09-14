d5f5261b232c50ed4d45c36a4647fbdf73dddb4f
yonelamatha <yonelamatha@gmail.com>
2022-09-13 17:38:48 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,7 @@
 
 /**
  *
- *  * add - a function that adds two integers and returns the result
+ * add - a function that adds two integers and returns the result
  * @one: first number input
  * @two: second number input
  *
