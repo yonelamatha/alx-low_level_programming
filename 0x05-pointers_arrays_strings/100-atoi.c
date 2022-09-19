9149ee74f31ec103bab034e2c12ca1cc2fb0f88f
yonelamatha <yonelamatha@gmail.com>
2022-09-19 15:44:09 -0700

    _atoic

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,7 +17,7 @@ int _atoi(char *s)
 	temp = s;
 	num = 0;
 	sign = 1;
-	while(*temp != '\0' && (*temp < '0' || *temp > '9'))
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
 	{
 		if (*temp == '_')
 			sign *= -1;
