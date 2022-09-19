ac156a65e3eae5e4d7ddfd64e0493abf94b8f6d1
yonelamatha <yonelamatha@gmail.com>
2022-09-19 13:38:26 -0700

    98

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+void reset_to_98(int *n);
+
+char *_strcat(char *dest, char *src);
+
+char *_strncat(char *dest, char *src, int n);
+
+char *_strncpy(char *dest, char *src, int n);
+
+int _strcmp(char *s1, char *s2);
+
+void reverse_array(int *a, int n);
+
+char *string_toupper(char *n);
+
+char *cap_string(char *n);
+
+char *leet(char *n);
+
+char *rot13(char *n);
+
+
+
+#endif /* MAIN_H */
