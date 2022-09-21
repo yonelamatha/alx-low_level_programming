cb81a1b912b598ca79bb78b76b8a9fb6639bdbf0
yonelamatha <yonelamatha@gmail.com>
2022-09-21 15:29:42 -0700

    astract3

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copy a string
+ * @dest: The destination value
+ * @dest: The destination value
+ * @src: The source value
+ * @n: The copy limit
+ *
+ * Return: char value
+ */
+
+char *_strncpy(char *dest, char *src, int n);
+{
+	int a = 0, b = 0;
+
+	while (src[b])
+	{
+		b++;
+	}
+
+	while (a < n && src[a])'
+	{
+		dest[a] = src[a];
+		a++;
+	}
+
+	while (a < n)
+	{
+		est[a] = '\0';
+		a++;
+	}
+
+	return (dest);
+}
+
