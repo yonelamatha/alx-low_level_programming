c1041e45136c3853cf6876f0d26d4047ff058880
yonelamatha <yonelamatha@gmail.com>
2022-09-26 11:09:00 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _meset - fills a memory block with a constant byte
+ * @s: address to memory block
+ * @b: char to be used
+ * @n: number of bytes to be used
+ *
+ * Return: pointer to the memory block
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	while (n)
+	{
+		s[n -1] = b;
+		n--;
+	}
+	return (s)
+}
