07f95ceda5a4be7d0e8628b8b89e9eaa7c7ed6ce
yonelamatha <yonelamatha@gmail.com>
2022-09-26 20:31:18 -0700

    task 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints a chessboard
+ * @a: pointer to matrix containing the chessboard
+ *
+ * Return: nothing
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(s[i][j]);
+		}
+		_putchar('\n');
+	}
+}
