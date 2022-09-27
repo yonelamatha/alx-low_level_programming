73a0136b790983c013bac40da4067b1c52a76de8
yonelamatha <yonelamatha@gmail.com>
2022-09-27 09:42:53 -0700

    task 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,17 +11,17 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j, p, l = 0, r = 0;
+	int i, j;
+	int d1 = 0, d2 = 0;
 
-	for (i =0; i < size; i++)
+	for (i = 0; i < size * size; i = i + (size + 1))
 	{
-		p = (i * size) + i;
-		l += *(a + p);
+		d1 = d1 + a[i];
 	}
-	for (j = 0; j < size; j++)
+	for (j = size - 1; j < size * size - 1; j = j + (size - 1))
 	{
-		p = (j * size) + (size - l - j);
-		r += *(a + p);
+		d2 = d2 + a[j];
 	}
-	printf("%i, %\n", l, r);
+	printf("%d, %d\n", d1, d2);
 }
+
