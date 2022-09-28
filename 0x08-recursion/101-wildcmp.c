0f1b3b703491b90d552b3d41065e64aa38b351a9
yonelamatha <yonelamatha@gmail.com>
2022-09-28 11:50:40 -0700

    task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -22,7 +22,7 @@ int wildcmp(char *s1, char *s2)
 		if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
 			return (0);
 		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
-			return (1):
+			return (1);
 	}
 	return (0);
 }
