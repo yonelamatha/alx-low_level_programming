e68e7cccdc0306307dfd23a1aed2b288b5e0e4cf
yonelamatha <yonelamatha@gmail.com>
2022-10-04 19:59:02 -0700

    task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -18,29 +18,29 @@ char *argstostr(int ac, char **av)
 	
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	
+
 	for (arg = 0; arg < ac; arg++)
 	{
 		for (byte = 0; av[arg][byte]; byte++)
 			size++;
 	}
 	str = malloc(sizeof(char) * size + 1);
-	
+
 	if (str == NULL)
 		return (NULL);
-	
+
 	index = 0;
-	
+
 	for (arg = 0; arg < ac; arg++)
 	{
 		for (byte = 0; av[arg][byte]; byte++)
 			str[index++] = av[arg][byte];
-		
+
 		str[index++] = '\n';
 	}
 	
 	str[size] = '\0';
-	
+
 	return (str);
 }
 
