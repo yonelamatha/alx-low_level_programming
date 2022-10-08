e3e08fc3deb9f9535cd888911eea64482fe5fe6a
yonelamatha <yonelamatha@gmail.com>
2022-10-07 20:48:17 -0700

    task 5

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - multiplies of two positive numbers
+ * @argc: n arguments
+ * @argv: args
+ *
+ * Retrurn: int
+ */
+
+int main(int argc, char *argv[])
+{
+	unsigned long mul;
+	int i, j;
+
+	if (argc != 3)
+	{
+		printf("error\n");
+		exit (98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for ( j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+				printf("error\n");
+			exit (98);
+		}
+
+	}
+	mul = atol(argv[1] *atol(argv[2]);
+			printf("%lu\n", mul);
+			return (0);
+}
+		
+
+
