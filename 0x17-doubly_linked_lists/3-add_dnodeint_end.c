410a9457eb37d22880dede561d96373e5b330c5c
yonelamatha <yonelamatha@gmail.com>
2023-01-06 02:18:52 -0800

    task

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,39 +1,20 @@
- #include "lists.h"
-
-/**
- * add_dnodeint_end - adds a new node at the end of a doubly linked list
- * @head: double pointer to the list
- * @n: data to insert in the new node
- *
- * Return: the address of the new element, or NULL if it failed
- */
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
-{
-	dlistint_t *new;
-	dlistint_t *temp = *head;
-
-	if (!head)
-		return (NULL);
-
-	new = malloc(sizeof(dlistint_t));
-	if (!new)
-		return (NULL);
-
-	new->n = n;
+= n;
 	new->next = NULL;
 
-	if (*head == NULL)
+	h = *head;
+
+	if (h != NULL)
 	{
-		new->prev = NULL;
+		while (h->next != NULL)
+			h = h->next;
+		h->next = new;
+	}
+	else
+	{
 		*head = new;
-		return (new);
 	}
 
-	while (temp->next)
-		temp = temp->next;
-
-	temp->next = new;
-	new->prev = temp;
+	new->prev = h;
 
 	return (new);
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,8 +1,8 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_
+#define _LISTS_
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -11,7 +11,7 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * for Holberton project
+ * for ALX project
  */
 typedef struct dlistint_s
 {
@@ -29,5 +29,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+
 
 #endif
