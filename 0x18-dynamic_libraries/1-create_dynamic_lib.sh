c1bef63fe1ae36779564f017b68dbe43c98895f5
yonelamatha <yonelamatha@gmail.com>
2023-01-07 21:43:31 -0800

    task

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c -fPIC *.c
+gcc -Wall -Wextra -werror -pedantic -c -fPIC *.c
 gcc -shared -o libdynamic.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
