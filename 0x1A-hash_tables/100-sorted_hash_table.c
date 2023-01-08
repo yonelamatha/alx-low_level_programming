7c792e4b06a4966a234d4e1933963af15d35c9ca
yonelamatha <yonelamatha@gmail.com>
2023-01-07 20:15:09 -0800

    task

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,5 +1,4 @@
-
-i#include "hash_tables.h"
+#include "hash_tables.h"
 
 shash_table_t *shash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
